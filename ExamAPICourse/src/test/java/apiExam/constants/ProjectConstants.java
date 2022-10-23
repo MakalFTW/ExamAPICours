8723dca116ac522100356b685d1c6140996baf3e
MakalFTW <112897751+MakalFTW@users.noreply.github.com>
2022-10-24 01:14:59 +0300

    Made fast configuring

---
+++ ExamAPICourse/src/test/java/apiExam/constants/ProjectConstants.java
@@ -0,0 +1,9 @@
+package apiExam.constants;
+
+public class ProjectConstants {
+
+    public static final String API_KEY = "";
+    public static final String API_TOKEN = "";
+    public static final String BOARD_ID = "";
+
+}
