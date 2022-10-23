e40900102136d98e3f7ef578ddb40d05e321f1ed
MakalFTW <112897751+MakalFTW@users.noreply.github.com>
2022-10-23 21:33:39 +0300

    Initial Commit

---
+++ untitled/src/test/java/apiExam/stepdefinitions/Hooks.java
@@ -0,0 +1,18 @@
+package apiExam.stepdefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+
+    @Before
+    public void beforeHook(){
+
+    }
+
+    @After
+    public void afterHook(){
+
+    }
+
+}
---
+++ untitled/src/test/java/apiExam/testRunner/TestRunner.java
@@ -0,0 +1,19 @@
+package apiExam.testRunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "junit:target/cucumber-reports/report.xml"
+        },
+        features = {"src/test/resources/features"},
+        glue = {"apiCourse/stepdefinitions"}
+)
+
+public class TestRunner {
+
+}
