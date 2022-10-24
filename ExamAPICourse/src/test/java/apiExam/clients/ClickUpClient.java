f90c7d1d7815cd0487add992606cf4757fb0cddb
MakalFTW <112897751+MakalFTW@users.noreply.github.com>
2022-10-24 12:41:27 +0300

    Skeleton upd

--- ExamAPICourse/src/test/java/apiExam/clients/ClickUpClient.java
+++ ExamAPICourse/src/test/java/apiExam/clients/ClickUpClient.java
@@ -1,5 +1,26 @@
 package apiExam.clients;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import static apiExam.constants.ProjectConstants.API_KEY;
+
 public class ClickUpClient {
+
+    private static RequestSpecification getRequests(){
+        return RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .header("Authorization", API_KEY)
+    }
+
+    public static Response getBoardInfo(){
+        return RestAssured
+                .given(getRequests())
+                .when()
+                .get("")
+    }
 
 }
